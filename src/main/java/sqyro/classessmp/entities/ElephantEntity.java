b16f1cae76bfebab40c3a75409a544d1d2b3663c
Sqyro <Ssqyro@gmail.com>
2026-08-09 22:55:31 +0200

    Made chunkbase and seed crackers not wor

--- src/main/java/sqyro/classessmp/entities/ElephantEntity.java
+++ src/main/java/sqyro/classessmp/entities/ElephantEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
 public class ElephantEntity extends PathfinderMob {
@@ -18,7 +19,7 @@ public class ElephantEntity extends PathfinderMob {
         this.goalSelector.addGoal(0, new FloatGoal(this));
         this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2D, false));
         this.goalSelector.addGoal(2, new RandomStrollGoal(this, 1.0D));
-        //this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
 
         this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
