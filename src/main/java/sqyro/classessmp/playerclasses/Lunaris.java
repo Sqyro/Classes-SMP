ff860b9ae34e5d0709c93b1ca55e83bc3e3bf916
Sqyro <Ssqyro@gmail.com>
2026-08-30 23:48:53 +0200

    -Lunaris Effects are applied

--- src/main/java/sqyro/classessmp/playerclasses/Lunaris.java
+++ src/main/java/sqyro/classessmp/playerclasses/Lunaris.java
@@ -1,6 +1,8 @@
 package sqyro.classessmp.playerclasses;
 
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import sqyro.classessmp.core.PlayerClass;
 
@@ -16,7 +18,15 @@ public class Lunaris extends PlayerClass {
 
     @Override
     public void onTick() {
+        float healthPercentage = Player.getHealth() / Player.getMaxHealth();
 
+        if (healthPercentage >= 0.80F) {
+            Player.addEffect(new MobEffectInstance(MobEffects.SPEED, 40, 2, false, false));
+            Player.addEffect(new MobEffectInstance(MobEffects.STRENGTH, 40, 2, false, false));
+        } else {
+            Player.addEffect(new MobEffectInstance(MobEffects.SPEED, 40, 1, false, false));
+            Player.addEffect(new MobEffectInstance(MobEffects.STRENGTH, 40, 1, false, false));
+        }
     }
 
     @Override
